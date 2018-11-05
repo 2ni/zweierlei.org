5fc1c218c37c569f11fae003ec5d3ba3f3e0bef1
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2018-11-05 09:50:25 +0100

    boilerplate to run one command to have all necessary terminals as wished for developing

---
+++ runterminals.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/osascript
+-- https://medium.com/@sunskyearthwind/iterm-applescript-and-jumping-quickly-into-your-workflow-1849beabb5f7
+-- https://www.iterm2.com/documentation-scripting.html
+
+tell application "iTerm2"
+  tell current window
+    create tab with default profile
+  end tell
+
+  tell first session of current tab of current window
+    split horizontally with default profile
+    write text "pwd"
+  end tell
+
+  tell second session of current tab of current window
+    write text "cd ~/Downloads"
+  end tell
+end tell
+
+tell application "iTerm2"
+  tell current window
+    create tab with default profile
+  end tell
+
+  tell first session of current tab of current window
+    write text "echo 'hello'"
+  end tell
+end tell
