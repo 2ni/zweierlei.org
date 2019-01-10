d89bcf8913b3f59d248185fae1df0f09be73cb6d
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2019-01-10 07:57:37 +0100

    correct indenting

--- frontend/src/store/alert.module.ts
+++ frontend/src/store/alert.module.ts
@@ -1,32 +1,32 @@
 export const alert = {
-    namespaced: true,
-    state: {
-        message: null,
-        type: null,
+  namespaced: true,
+  state: {
+    message: null,
+    type: null,
+  },
+  actions: {
+    success({ commit }, message: any) {
+      commit('success', message);
     },
-    actions: {
-        success({ commit }, message: any) {
-            commit('success', message);
-        },
-        error({ commit }, message: any) {
-            commit('error', message);
-        },
-        clear({ commit }, message: any) {
-            commit('success', message);
-        },
+    error({ commit }, message: any) {
+      commit('error', message);
     },
-    mutations: {
-        success(state: any, message: any) {
-            state.type = 'success';
-            state.message = message;
-        },
-        error(state: any, message: any) {
-            state.type = 'danger';
-            state.message = message;
-        },
-        clear(state: any) {
-            state.type = null;
-            state.message = null;
-        },
+    clear({ commit }, message: any) {
+      commit('success', message);
     },
+  },
+  mutations: {
+    success(state: any, message: any) {
+      state.type = 'success';
+      state.message = message;
+    },
+    error(state: any, message: any) {
+      state.type = 'danger';
+      state.message = message;
+    },
+    clear(state: any) {
+      state.type = null;
+      state.message = null;
+    },
+  },
 };
